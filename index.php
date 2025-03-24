5ea6d926628c5b81e1bb01aba9638ba01e9371fe
dariusston <shalomdogbevi01@gmail.com>
2025-03-24 15:35:24 +0100

    php

--- index.php
+++ index.php
@@ -5,23 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Started PHP</title>
-	<link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
-    <link rel="manifest" href="../favicon/site.webmanifest">
-    <link rel="stylesheet" href="../css/style.css">
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 	<!--cours https://hackmd.io/iNktz6XzRyu7gGmKvPpXfQ?view-->
 	<header>
 		<h1>
 			🚀 
-			PHP started version 8
+			PHP started version 8.2
 		</h1>
 	</header>
 	<main>
         <figure>
-            <img src="../asset/cover.svg" alt="cover php">
+            <img src="./asset/cover.svg" alt="cover php">
             <figcaption>
                 <h2>
                     PHP version 8
